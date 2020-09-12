e606688814a1814e9489c2b0a95b6a5ae19c0d39
hrykoda <hrykoda@gmail.com>
2020-09-12 12:46:06 +0900

    add file

---
+++ netdump.sh
@@ -0,0 +1,24 @@
+#/bin/sh
+
+# <参考URL>
+# tcpdumpでキャプチャファイルを定期的にローテートさせる方法
+# https://www.checksite.jp/tcpdump-rotate-option/
+#
+# <ファイル一覧コマンド>
+# ls --full-time capture/*
+
+# 固定値設定
+CAPDIR=capture
+IF=eth0
+ROTATE=10
+
+# 変数設定
+CAPUSER=`id -un`
+TS=`date "+%Y%m%d_%H%M%S"`
+
+# ディレクトリ作成（なければ）
+mkdir -p ${CAPDIR}
+
+# キャプチャ取得
+#sudo tcpdump -i ${IF} -e > ${CAPDIR}/${IF}-${TS}.txt
+sudo tcpdump -i ${IF} -Z ${CAPUSER} -G ${ROTATE} -w ${CAPDIR}/netdump-${IF}-${TS}-%y%m%d_%H%M%S.cap
